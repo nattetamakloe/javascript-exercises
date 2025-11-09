4be3f4f3b0f109d6f71c3ea958761762f6f6b5c7
N8DGR8 <38409384+nattetamakloe@users.noreply.github.com>
2025-11-09 21:18:01 +0100

    Add function removeFromArray

--- 08_removeFromArray/removeFromArray.js
+++ 08_removeFromArray/removeFromArray.js
@@ -1,4 +1,10 @@
-const removeFromArray = function() {
+const removeFromArray = function(array, remove) {
+    for (let i = 0; i < array.length; i++) {
+        if (array[i] == remove) {
+            array.splice(i, 1);
+        }
+    }
+    return array
 };
 
 // Do not edit below this line
